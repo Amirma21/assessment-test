ca34a21334e41fb973a727a75979339afffaca70
Amir Maleki <amirmaleki21physics@gmail.com>
2022-05-15 00:44:58 +0430

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
 import './App.css';
+import CountProvider from './components/Context/CountProvider';
+import Body from './components/Layout/Body/Body';
+import Footer from './components/Layout/Footer/Footer';
+import Header from './components/Layout/Header/Header';
 
 function App() {
   return (
-   <h2>hello</h2>
+    <>
+      <CountProvider>
+        <Header></Header>
+        <Body></Body>
+        <Footer></Footer>
+      </CountProvider>
+    </>
   );
 }
 
