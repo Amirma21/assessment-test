81134cf60e537824fdc7516867d96311c5241436
Amir Maleki <amirmaleki21physics@gmail.com>
2022-05-15 00:13:41 +0430

    body style

--- src/components/layout/Body/Body.style.js
+++ src/components/layout/Body/Body.style.js
@@ -15,19 +15,44 @@ border-radius: 15px;
 ${({ isExpand }) => !isExpand ? `width: 5%;` : `width:25%`}
 `
 
-export const CountdownContainer = styled.div`
-width: 70%;
-height: 80%;
-background-color: #a855f7;
-border-radius: 15px;
-
-`
-
 export const IconContainer = styled.div`
 display: inline-block;
 font-size:30px;
 cursor: pointer;
 margin: 5px 0 0 10px;
-color: #c7d2fe;
+color: #e2e8f0;
 font-weight: bold;
+`
+
+export const CountdownContainer = styled.div`
+width: 70%;
+height: 80%;
+background-color: #a855f7;
+border-radius: 15px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-around;
+`
+
+export const Title = styled.h2`
+font-size: 30px;
+font-weight: bold;
+color: #e2e8f0;
+letter-spacing: 2px;
+margin: 0;
+`
+
+export const Indicator = styled.div`
+width: 60%;
+height: 8px;
+background-color: #94a3b8;
+border-radius: 4px;
+`
+
+export const Number = styled.span`
+font-size: 40px;
+font-weight: bold;
+color:#e2e8f0;
+/* ${({ count }) => !count > 50 ? `color:red` : `color:black`} */
 `
