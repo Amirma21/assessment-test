077587c41658410fa4ade8276b55fe7a798ea468
Amir Maleki <amirmaleki21physics@gmail.com>
2022-05-15 00:31:51 +0430

    footer

---
+++ src/components/layout/Footer/Footer.js
@@ -0,0 +1,13 @@
+import { FooterContainer , Text } from "./Footer.style";
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+           <Text>
+               FOOTER
+           </Text>
+        </FooterContainer>
+    );
+}
+ 
+export default Footer;
---
+++ src/components/layout/Footer/Footer.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+ export const FooterContainer = styled.div`
+width: 100%;
+height: 200px;
+background-color: #fff;
+box-shadow: rgb(0 0 0 / 5%) 0px 6px 24px 0px, rgb(0 0 0 / 8%) 0px 0px 0px 1px;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const Text = styled.h2`
+font-size: 30px;
+font-weight: bold;
+opacity: 0.4;
+`
