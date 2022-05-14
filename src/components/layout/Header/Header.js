2e9a00c3ac93044f4ffc0480367b7ffd91852b53
Amir Maleki <amirmaleki21physics@gmail.com>
2022-05-15 00:13:00 +0430

    counter in Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,10 +1,13 @@
 import { TimeContainer, NavbarContainer, Text } from "./Header.style";
+import { useCount } from "../../Context/CountProvider"
 
 const Header = () => {
+    const counter = useCount()
+
     return (
         <NavbarContainer>
             <TimeContainer>
-               <Text> The time left :</Text>
+                <Text> The time left : {counter}</Text>
             </TimeContainer>
         </NavbarContainer>
     );
