52eff3fc929ec03eddd4bfbddae121fe76e13546
Amir Maleki <amirmaleki21physics@gmail.com>
2022-05-15 19:15:01 +0430

    use ../src iinstead ./src in import files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 
 import './App.css';
 import CountProvider from './components/Context/CountProvider';
-import Body from './components/Layout/Body/Body';
-import Footer from './components/Layout/Footer/Footer';
-import Header from './components/Layout/Header/Header';
+import Body from '../src/components/Layout/Body/Body';
+import Footer from '../src/components/Layout/Footer/Footer';
+import Header from '../src/components/Layout/Header/Header';
 
 function App() {
   return (
     <>
       <CountProvider>
-        <Header></Header>
-        <Body></Body>
-        <Footer></Footer>
+        <Header/>
+        <Body/>
+        <Footer/>
+     
       </CountProvider>
     </>
   );
